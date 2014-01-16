55409df52d840ee19ebff5fcc3000030b655a339
stevenlauwers22 <stevenlauwers22@hotmail.com>
2014-01-16 12:33:37 +0100

    Move jar file to root

--- Default.js
+++ Default.js
@@ -2,25 +2,22 @@
 Contribute.eid = Contribute.eid || {};
 
 Contribute.eid.uitlezen = function() {
-//    alert('TODO: uitlezen');
 	var attributes = {
-	    codebase: 'https://contribute-eid-poc.apphb.com/',
-            code :'be.fedict.eid.applet.Applet.class',
-            archive :'applet/eid-applet-package-1.1.3.jar',
-            width :600,
-            height :300
-        };
+        code :'be.fedict.eid.applet.Applet.class',
+        archive: 'eid-applet-package-1.1.3.jar',
+        width :600,
+        height :300
+    };
 
-        var parameters = {
-            //TargetPage :'http://contribute-eid-poc.apphb.com/',
-            TargetPage :'https://tools.contribute.be/ingpoc/showEidData',
-            AppletService :'https://tools.contribute.be/ingpoc/applet-service;jsessionid=<%=session.getId()%>',
-            BackgroundColor :'#ffffff',
-            Language : 'en',
-            HideDetailsButton : true
-        };
-        var version = '1.6';
-        deployJava.runApplet(attributes, parameters, version);
+    var parameters = {
+        TargetPage :'https://tools.contribute.be/ingpoc/showEidData',
+        AppletService :'https://tools.contribute.be/ingpoc/applet-service;jsessionid=<%=session.getId()%>',
+        BackgroundColor :'#ffffff',
+        Language : 'en',
+        HideDetailsButton : true
+    };
+    var version = '1.6';
+    deployJava.runApplet(attributes, parameters, version);
 };
 
 Contribute.eid.uitlezenMetCode = function() {
