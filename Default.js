2b3fe95399c79d278a9e94fc13562c6409bae1aa
koenschepers <koenschepers@gmail.com>
2014-02-03 10:53:51 +0100

    read eid with pin

--- Default.js
+++ Default.js
@@ -10,7 +10,7 @@ Contribute.eid.uitlezen = function(sessionId) {
     };
 
     var parameters = {
-        TargetPage :'https://tools.contribute.be/ingpoc/showEidData',
+        TargetPage :'https://qfbam.cronos.be',
         AppletService :'https://tools.contribute.be/ingpoc/applet-service;jsessionid=' + sessionId,
         BackgroundColor :'#ffffff',
         Language : 'en',
@@ -21,5 +21,20 @@ Contribute.eid.uitlezen = function(sessionId) {
 };
 
 Contribute.eid.uitlezenMetCode = function() {
-    alert('TODO: uitlezen met code');
+    var attributes = {
+		code :'be.fedict.eid.applet.Applet.class',
+		archive :'resources/jar/eid-applet-package-1.1.3.jar',
+		width :1,
+		height :1
+	};
+
+	var parameters = {
+		TargetPage :'https://qfbam.cronos.be',
+		AppletService :'https://tools.contribute.be/ingpoc/authentication-service;jsessionid=' + sessionId,
+		BackgroundColor :'#ffffff',
+		Language : 'en',
+		HideDetailsButton : true
+	};
+	var version = '1.6';
+	deployJava.runApplet(attributes, parameters, version);
 };
